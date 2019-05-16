726428a33bce7d3e4975909bb8e8a5779b14b415
Paolo Lammens <lammenspaolo@gmail.com>
2019-05-16 15:51:23 +0200

    refactor: error public virtual bases, `MatrixError`

--- include/errors.h
+++ include/errors.h
@@ -11,20 +11,27 @@
 
 /// Abstract base exception class
 class BaseException : public std::exception {
-protected:
+public:
     std::string message;  ///< message to be returned by what()
 
     BaseException() = default;
     explicit BaseException(const char *baseMessage, const std::string &details);
+    ~BaseException() noexcept override = 0;
 
-public:
     /// `std::exception`-compliant message getter
     const char *what() const noexcept override { return message.c_str(); }
 };
 
 
+class MatrixError : public virtual BaseException {
+public:
+    static constexpr auto base = "invalid value";
+    explicit MatrixError(const char *specific = nullptr) : BaseException(base, specific) {}
+};
+
+
 /// Indicates that an algorithm encountered a singular matrix
-class SingularMatrixError : public BaseException {
+class SingularMatrixError : public virtual MatrixError {
 public:
     static constexpr auto baseMessage = "singular matrix";
     explicit SingularMatrixError(double tol, const std::string &details = "");
@@ -33,20 +40,20 @@ private:
     static std::string getDetails(const std::string &details, double tol);
 };
 
-class ValueError : public BaseException {
+class ValueError : public virtual BaseException {
 public:
     static constexpr auto base = "invalid value";
     explicit ValueError(const char *specific = nullptr) : BaseException(base, specific) {}
 };
 
-class IOError : public BaseException {
+class IOError : public virtual BaseException {
 public:
     static constexpr auto base = "unable to access file";
     explicit IOError(const char *specific = nullptr) : BaseException(base, specific) {}
 };
 
 
-class BadFormat : public BaseException {
+class BadFormat : public virtual BaseException {
 public:
     static constexpr auto base = "bad input format";
     explicit BadFormat(const char *specific = nullptr) : BaseException(base, specific) {}
